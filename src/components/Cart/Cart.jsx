aa58d46eaf30975d09e04bf379a43640cd50fe25
ehsanfuad <ehsanfuad1402@gmail.com>
2023-05-03 11:52:37 +0330

    twelvth commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,11 @@
-import { Box, Popover, Typography, Link } from "@mui/material";
-import React, { useState } from "react";
+import { Box, Popover, Typography, Link, Button, Divider } from "@mui/material";
+import React from "react";
 import useClasses from "../../utils/useClasses";
 import { Link as RouterLink } from "react-router-dom";
-import { theme } from "../../data/dummy";
+import { allProducts, theme } from "../../data/dummy";
 import { FiChevronLeft } from "react-icons/fi";
+import "./Cart.scss";
+import ProductCard from "./ProductCard";
 
 function Cart({ setCartAnchorEl, cartAnchorEl, eventCart, isShowCart }) {
   const open = Boolean(cartAnchorEl);
@@ -26,30 +28,6 @@ function Cart({ setCartAnchorEl, cartAnchorEl, eventCart, isShowCart }) {
   });
   const classes = useClasses(styles);
 
-  const ProductCard = (image, title) => {
-    return (
-      <Box display="flex" flexDirection="row" gap={2}>
-        <Box
-          bgcolor="deepskyblue"
-          flex={1}
-          display="flex"
-          flexDirection="column"
-        >
-          <img
-            src="https://dkstatics-public.digikala.com/digikala-products/2108647c04c8ff8206c7ba86aceb8b4173f06fcf_1656429173.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/format,webp/quality,q_80"
-            width="100%"
-            alt={title}
-          />
-        </Box>
-        <Box bgcolor="azure" flex={2} display="flex" flexDirection="column">
-          <Typography fontSize="0.9rem" lineHeight={2}>
-            گوشی موبایل اپل مدل iPhone 13 Pro دو سیم‌ کارت ظرفیت 1 ترابایت و 6
-            گیگابایت رم - نات اکتیو
-          </Typography>
-        </Box>
-      </Box>
-    );
-  };
   const CartContent = () => {
     return (
       <Popover
@@ -72,7 +50,6 @@ function Cart({ setCartAnchorEl, cartAnchorEl, eventCart, isShowCart }) {
         }}
       >
         <Box
-          px={2}
           pt={1}
           display="flex"
           flexDirection="column"
@@ -83,6 +60,8 @@ function Cart({ setCartAnchorEl, cartAnchorEl, eventCart, isShowCart }) {
             display="flex"
             alignItems="center"
             justifyContent="space-between"
+            height="30px"
+            px={2}
           >
             <Typography
               fontSize="0.8rem"
@@ -95,7 +74,34 @@ function Cart({ setCartAnchorEl, cartAnchorEl, eventCart, isShowCart }) {
               </Box>
             </Link>
           </Box>
-          <ProductCard />
+          <Box sx={{ overflowY: "scroll", height: "390px" }} px={2}>
+            {allProducts.slice(0, 4).map((product, index) => (
+              <ProductCard product={product} key={index} />
+            ))}
+          </Box>
+          <Divider flexItem />
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+            height="80px"
+            px={2}
+          >
+            <Box display="flex" flexDirection="column">
+              <Typography color={theme.palette.grey[600]} variant="subtitle2">
+                مبلغ قابل پرداخت
+              </Typography>
+              <Typography fontSize={"1.25rem"}> ۱۳،۸۰۰،۰۰۰ تومان</Typography>
+            </Box>
+            <Button
+              sx={{ paddingY: "0.7rem" }}
+              variant="contained"
+              color="primary"
+            >
+              ورود و ثبت سفارش
+            </Button>
+          </Box>
         </Box>
       </Popover>
     );

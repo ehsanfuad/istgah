85097ce2c57b421eda03858afa58e084e068d750
ehsanfuad <ehsanfuad1402@gmail.com>
2023-05-18 11:43:34 +0330

    21th commit

--- src/Pages/Profile/Addresses.jsx
+++ src/Pages/Profile/Addresses.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Box, Button, Modal, Typography } from "@mui/material";
+import React, { useState } from "react";
+import Map from "./components/Map";
 
 function Addresses() {
-  return <div>Addresses</div>;
+  const [openMap, setOpenMap] = useState(false);
+  const handleOpenMap = () => {
+    setOpenMap(true);
+  };
+  const handleCloseMap = () => {
+    setOpenMap(false);
+  };
+  return (
+    <Box display="flex" flexDirection="column">
+      <Box display="flex" justifyContent="space-between">
+        <Typography>آدرس ها</Typography>
+        <Button variant="contained" onClick={() => handleOpenMap()}>
+          ثبت آدرس جدید
+        </Button>
+      </Box>
+      <Modal open={openMap} onClose={handleCloseMap}>
+        <Map />
+      </Modal>
+    </Box>
+  );
 }
 
 export default Addresses;
